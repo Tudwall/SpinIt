5558e84d2671ff44240513449ccbfc7980aa87cd
Pierre <pierre.laffaille@outlook.fr>
2024-12-06 12:06:20 +0100

    first try at displaying data from API

--- js/app.js
+++ js/app.js
@@ -31,13 +31,65 @@ releases: [
 }
 */
 
-fetch("https://api.discogs.com/artists/1/releases?page=1&per_page=1")
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error("connection failed");
+const APICall = async (url) => {
+	let data = await fetch(url);
+	let dataJson = await data.json();
+	return dataJson.releases;
+};
+
+const getData = async (url) => {
+	let jsonData = await APICall(url);
+	return jsonData;
+};
+
+const createCard = async (url) => {
+	const data = await getData(url);
+	data.forEach((element) => {
+		const card = document.createElement("div");
+		card.classList.add("card");
+
+		const imageEl = document.createElement("img");
+		imageEl.classList.add("cover-art");
+		imageEl.setAttribute("src", "assets/icons/Record.svg");
+
+		const infoEl = document.createElement("div");
+		infoEl.classList.add("info");
+
+		const artistEl = document.createElement("span");
+		artistEl.classList.add("artist");
+		artistEl.textContent = element.artist;
+		const albumEl = document.createElement("span");
+		albumEl.classList.add("album");
+		albumEl.textContent = element.title;
+		const dateEl = document.createElement("span");
+		dateEl.classList.add("date");
+		dateEl.textContent = element.year;
+
+		infoEl.appendChild(artistEl, albumEl, dateEl);
+
+		const actionsEl = document.createElement("div");
+		actionsEl.classList.add("actions");
+		const starsEl = document.createElement("div");
+		starsEl.classList.add("stars");
+		for (let i = 0; i < 5; i++) {
+			const star = document.createElement("img");
+			star.setAttribute("src", "assets/icons/Star-fill.svg");
+			starsEl.appendChild(star);
 		}
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
+		actionsEl.appendChild(starsEl);
+
+		const addEl = document.createElement("div");
+		const addCollec = document.createElement("img");
+		addCollec.setAttribute("src", "assets/icons/Add.svg");
+		addCollec.classList.add("add-collection");
+		const addWish = document.createElement("img");
+		addWish.setAttribute("src", "assets/icons/Heart-outline.svg");
+		addWish.classList.add("add-wishlist");
+		addEl.appendChild(addCollec, addWish);
+
+		card.appendChild(infoEl, actionsEl);
+		document.getElementById("card-container").appendChild(card);
 	});
+};
+
+createCard("https://api.discogs.com/artists/1/releases?page=1&per_page=1");
